de76836486d496da8018f243d9447d4539a0c674
hangu <hangu1116@gmail.com>
2018-03-12 22:52:22 +0800

    Add files via upload

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,1 +1,33 @@
+//logs.js
+const util = require('../../utils/util.js')
 
+Page({
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+  },
+  canvasIdErrorCallback: function (e) {
+    console.error(e.detail.errMsg)
+  },
+  onReady: function (e) {
+    // 使用 wx.createContext 获取绘图上下文 context
+    var context = wx.createCanvasContext('firstCanvas')
+    
+    context.setStrokeStyle("#00ff00")
+    context.setLineWidth(5)
+    context.rect(0, 0, 200, 200)
+    context.stroke()
+
+    var img = String(wx.getStorageSync('logs'))
+    console.log(img)
+    context.drawImage(img, 0, 0, 200, 200)
+
+    // context.setStrokeStyle("#ff0000")
+    // context.setLineWidth(2)
+    // context.moveTo(160, 100)
+    // context.arc(100, 100, 60, 0, 2 * Math.PI, true)
+    // context.stroke()
+    context.draw()
+  }
+})
